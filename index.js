f78d1fa2c62c1e4f4eda1202253caa333a74718e
Abhishekballi13 <43747581+Abhishekballi13@users.noreply.github.com>
2022-09-17 22:38:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // it is same as prv dice step we have just adjusted the address in single var
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Play 1 wins!";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML = "Play 2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
